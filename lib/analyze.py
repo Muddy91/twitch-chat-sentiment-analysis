7233d3787623fb52c40d3e76a980feabb52b7354
evanlissoos <evanlissoos@gmail.com>
2016-08-13 18:51:42 -0700

    Added a catch for catching API errors

--- lib/analyze.py
+++ lib/analyze.py
@@ -4,6 +4,8 @@ from lib.alchemyapi import AlchemyAPI
 meta_collection = []
 #Setting up API for sentiment analysis
 alchemyapi = AlchemyAPI()
+#Counter for checking if API is still responsive
+error_counter = 0
 
 
 #Function to perform sentiment analysis on a given chat message
@@ -29,14 +31,20 @@ def update(index, sentiment, confidence):
     if sentiment == 'positive':
         meta_collection[index]['pos'] += 1
         meta_collection[index]['posCon'] += confidence
+        error_counter = 0
 
     elif sentiment == 'neutral':
         meta_collection[index]['neu'] += 1
         meta_collection[index]['neuCon'] += confidence
+        error_counter = 0
 
     elif sentiment == 'negative':
         meta_collection[index]['neg'] += 1
         meta_collection[index]['negCon'] += confidence
+        error_counter = 0
 
     else:
         meta_collection[index]['Error'] += 1
+        error_counter += 1
+        if error_counter == MAX_ERROR_COUNT:
+            print 'Recieved too many error messages from the API\nExiting program...'
--- lib/config.py
+++ lib/config.py
@@ -10,5 +10,6 @@ CHANNEL = "#ster"	#Twitch channel to read chat from
 #Analytics variables
 REFRESH_RATE = 0	#Time in seconds that the process will sleep to check for another message
 ANALYSIS_TIME_FRAME = 60	#Time in seconds that the program will perform meta-data analysis
+MAX_ERROR_COUNT = 10	#Maximum amount of errors the API can send in a row before exiting the program
 CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")	#Reg expression for filtering out chat messages
 META_EMPTY = {'pos': 0, 'neg' : 0, 'neu' : 0, 'posCon' : 0, 'negCon' : 0, 'neuCon' : 0, 'Error' : 0}	#Empty dictionary for keeping track of live meta data
